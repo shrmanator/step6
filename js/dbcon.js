7b73b7d5b684dfe5df949e9b42e9071ef689999f
Dov <dovindustries@gmail.com>
2021-02-27 20:15:42 -0500

    added new file 'users.js' which will get the info from the db. it still need to be populated.

--- js/dbcon.js
+++ js/dbcon.js
@@ -11,13 +11,30 @@ const pool = dbcon.createPool({
     database        : 'cs340_shermado'
 }); module.exports.pool = pool;  // pool.* will be all your server interfacing with the databse
 
+<<<<<<< HEAD
+=======
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+>>>>>>> 0ade028 (added new file 'users.js' which will get the info from the db. it still need to be populated.)
 let getConnection = function(callback) {
     pool.getConnection(function(err, connection) {
         callback(err, connection);
     });
 };
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0ade028 (added new file 'users.js' which will get the info from the db. it still need to be populated.)
 app.listen(port, function(err) {
     if (err) {console.log("error")}
 });
--- js/queries.js
+++ js/queries.js
@@ -26,6 +26,7 @@ app.get('/', function (req, res) {
     res.render('index', {});
 });
 
+<<<<<<< HEAD
 // Insert new UserAcccount into db
 app.post("/UserRegistration", (req, res) => {
     let mysql = req.app.get('mysql');
@@ -142,6 +143,125 @@ app.post("/deleteUserAccount", (req, res) => {
         res.send(queryResults);
     });
 });
+=======
+
+// // Insert new UserAcccount into db
+// app.post("/UserRegistration", (req, res) => {
+//     let mysql = req.app.get('mysql');
+//     let sql = `INSERT INTO UserRegistrations (lastName, firstName, password, email, zipCode) VALUES (?, ?, ?, ?, ?)`;
+//     let render = [req.body.lastName, req.body.firstName, req.body.password, req.body.email, req.body.zipCode];
+//     mysql.pool.query(sql, inserts, function (error) {
+//         let msg = ""
+//         if (error) {
+//             let msg = "incorrect info"
+//         } else {
+//             let msg = ` ${req.body.firstName}, your email is ${req.body.email} and your account is ready to use.`;
+//         }
+//         res.render('UserSignUp', {});
+//     });
+// });
+//
+// // Select a user from the UserAccounts table given email and password.
+// app.get("/UserLogin", (req, res) => {
+//     let mysql = req.app.get('mysql');
+//     let sql = `SELECT userID, lastName, firstName, email FROM UserSignUps WHERE email = ? AND password = ?`;
+//     let data = [req.query.email, req.query.password];
+//     mysql.pool.query(sql, data, function (error, results, fields) {
+//         const queryResults = {};
+//         if (error || results[0] == null) {
+//             queryResults["successful"] = false;
+//         } else {
+//             queryResults["successful"] = true;
+//             queryResults["userID"] = results[0].userID;
+//             queryResults["firstName"] = results[0].firstName;
+//             queryResults["lastName"] = results[0].lastName;
+//             queryResults["email"] = results[0].email;
+//         }
+//         res.render('UserLogin', {data});
+//     });
+// });
+//
+//
+// // Insert new Location into UserLocationsHistory:
+// app.post("/UserRegistration", (req, res) => {
+//     let mysql = req.app.get('mysql');
+//     let sql = `INSERT INTO UserLocationHistory VALUES (?, ?, ?)`;
+//     let inserts = [req.body.zipCode]
+//     mysql.pool.query(sql, inserts, function (error, results, fields) {
+//         let msg = ""
+//         if (error) {
+//             let msg = "incorrect info"
+//         } else {
+//             let msg = ` ${req.body.firstName}, your email is ${req.body.email} and your account is ready to use.`;
+//         }
+//         res.send({ msg });
+//     });
+// });
+//
+// // Select specific user:
+// app.post("/getUser", (req, res) => {
+//     let mysql = req.app.get('mysql');
+//     let sql = `SELECT UserAccounts.userID FROM UserAccounts WHERE UserAccounts.userID = ?`;
+//     let insert = [req.body.userID]
+//     mysql.pool.query(sql, insert, function (error, results, fields) {
+//         let queryResults = [];
+//         results.forEach((row) => {
+//             queryResults.push(row)
+//         })
+//         console.log(queryResults);
+//         res.send(queryResults);
+//     });
+// });
+//
+//
+// // Select all Guides in certain location:
+// app.post("/getGuides", (req, res) => {
+//     let mysql = req.app.get('mysql');
+//     let sql = `SELECT UserAccounts.isGuide FROM UserAccounts WHERE UserAccounts.locationID = ?`;
+//     let insert = [req.body.userID]
+//     mysql.pool.query(sql, insert, function (error, results, fields) {
+//         let queryResults = [];
+//         results.forEach((row) => {
+//             queryResults.push(row)
+//         })
+//         console.log(queryResults);
+//         res.send(queryResults);
+//     });
+// });
+//
+//
+//
+// // Delete a specific UserLocation:
+// app.post("/deleteUserLocation", (req, res) => {
+//     let mysql = req.app.get('mysql');
+//     let sql = `DELETE FROM UserLocationHistory WHERE UserLocationHistory.locationID = ?`;
+//     let insert = [req.body.userID]
+//     mysql.pool.query(sql, insert, function (error, results, fields) {
+//         let queryResults = [];
+//         results.forEach((row) => {
+//             queryResults.push(row)
+//         })
+//         console.log(queryResults);
+//         res.send(queryResults);
+//     });
+// });
+//
+//
+// // Delete a specific UserAccount:
+// app.post("/deleteUserAccount", (req, res) => {
+//     let mysql = req.app.get('mysql');
+//     let sql = `DELETE FROM UserLogins WHERE UserLogins.lastName = ? AND UserLogins.firstName = ? AND UserLogins.password = ?`;
+//     let insert = [req.body.userID]
+//     mysql.pool.query(sql, insert, function (error, results, fields) {
+//         let queryResults = [];
+//         results.forEach((row) => {
+//             queryResults.push(row)
+//         })
+//         console.log(queryResults);
+//         res.send(queryResults);
+//     });
+// });
+>>>>>>> 0ade028 (added new file 'users.js' which will get the info from the db. it still need to be populated.)
 
 app.set('port', process.argv[2]);
 
